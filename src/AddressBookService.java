c130aa39c8afe1b47c5774f03c1247d8983ff40a
RAAGUL VIGNESH <rv0815@srmist.edu.in>
2024-02-14 12:04:15 +0530

    [Raagul] Add : Added REGEX usecases to the Address Book

--- src/AddressBookService.java
+++ src/AddressBookService.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class AddressBookService {
+    private static final String EMAIL_REGEX =
+            "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@" +
+                    "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    private static final String Phno_Regex="^[6-9]{1}[0-9]{9}";
+    private static final String Pin_Regex="^[0-9]{6}";
     public String display(Person p){
+
         return p.getFirstName()+" "+p.getLastName()+" "+p.getCity()+" "+p.getState()+" "+ p.getEmail()+" "+p.getPhoneNumber()+ " "+p.getZip();
     }
     public void setValues(Person p){
@@ -19,12 +28,39 @@ public class AddressBookService {
         p.setState(state);
         System.out.println("Enter Email");
         String email = sc.next();
+        Pattern pattern = Pattern.compile(EMAIL_REGEX);
+        Matcher matcher = pattern.matcher(email);
+        while(!matcher.matches()){
+            System.out.println("Enter ur Email again");
+            String z=sc.next();
+            p.setEmail(z);
+            matcher=pattern.matcher(z);
+
+        }
         p.setEmail(email);
+
         System.out.println("Enter Phone Number");
-        int phoneNo = sc.nextInt();
+        String phoneNo = sc.next();
+        Pattern patterns = Pattern.compile(Phno_Regex);
+        Matcher matchers = patterns.matcher(phoneNo);
+        while(!matchers.matches()){
+            System.out.println("Enter ur phno again");
+            String phone=sc.next();
+            p.setEmail(phone);
+            matchers=patterns.matcher(phone);
+
+        }
         p.setPhoneNumber(phoneNo);
         System.out.println("Enter zip");
-        int zip = sc.nextInt();
+        String zip = sc.next();
+        Pattern patter = Pattern.compile(Pin_Regex);
+        Matcher matche = patter.matcher(zip);
+        while(!matche.matches()){
+            System.out.println("Enter ur pincode again");
+            String pin=sc.next();
+            p.setEmail(pin);
+            matche=patter.matcher(pin);
+        }
         p.setZip(zip);
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -3,8 +3,8 @@ public class Person {
     private String lastName;
     private String city;
     private String state;
-    private int zip;
-    private int phoneNumber;
+    private String zip;
+    private String phoneNumber;
     private String email;
 
     public String getFirstName() {
@@ -39,19 +39,19 @@ public class Person {
         this.state = state;
     }
 
-    public int getZip() {
+    public String getZip() {
         return zip;
     }
 
-    public void setZip(int zip) {
+    public void setZip(String zip) {
         this.zip = zip;
     }
 
-    public int getPhoneNumber() {
+    public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(int phoneNumber) {
+    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
